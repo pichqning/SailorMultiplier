fb4ab6b68fe733daae119d49db19e78cf80e8c1e
benzsupaluk <supaluk.j@ku.th>
2019-04-25 15:37:54 +0700

    Connect to database with UI

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import program.User;
+
+public class LoginController {
+
+    @FXML
+    TableColumn<User, String> usernameListCol;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    public void showUsernameList() {
+        
+    }
+}
