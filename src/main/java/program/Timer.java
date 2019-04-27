6c848d673a360cfbfbdcd4f375321749f15340f1
Pichaaun Popukdee <pichaaun.p@ku.th>
2019-04-27 19:42:31 +0700

    add boolean canplay in timer

--- src/main/java/program/Timer.java
+++ src/main/java/program/Timer.java
@@ -7,21 +7,31 @@ public class Timer{
 
     public Timer() {
         this.second = 60;
+        this.canPlay = true;
     }
 
-    public void setSecond(int second) {
-        this.second = second;
-    }
 
     public void start()   {
         for (int i = second ; i >= 0 ; i-- ){
             try {
                 Thread.sleep(1000);
+                System.out.println(i);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            
         }
+        canPlay = false;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public boolean getCanplay() {
+        return canPlay;
+    }
+    public void reset () {
+        this.canPlay = true;
     }
 }
 
