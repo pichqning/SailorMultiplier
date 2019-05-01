0bba746d7a73530cdb447afe590042f7b6dbca1a
benzsupaluk <supaluk.j@ku.th>
2019-05-01 20:45:46 +0700

    Edited game homepage

--- src/main/java/controller/SelectQuestionController.java
+++ src/main/java/controller/SelectQuestionController.java
@@ -18,7 +18,10 @@ import program.User;
 public class SelectQuestionController {
 
     @FXML
-    private Button h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, logoutButton;
+    private Button b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, logoutButton;
+
+    @FXML
+    private Label u2, u3, u4, u5, u6, u7, u8, u9, u10, u11, u12, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12;
 
     @FXML
     private AnchorPane selectQuesPane;
@@ -46,7 +49,7 @@ public class SelectQuestionController {
         user = LoginController.getUser();
         usernameLogin.setText(user.getUsername());
         setImgSailor();
-        setHighScoreToButton();
+        setTextLabel();
     }
 
     @FXML
@@ -64,23 +67,39 @@ public class SelectQuestionController {
         ChangePageManager.setUI(this.getClass(), "/UI/LoginUI.fxml");
     }
 
-    private void setHighScoreToButton() {
-        h2.setText(setTextButton(2));
-        h3.setText(setTextButton(3));
-        h4.setText(setTextButton(4));
-        h5.setText(setTextButton(5));
-        h6.setText(setTextButton(6));
-        h7.setText(setTextButton(7));
-        h8.setText(setTextButton(8));
-        h9.setText(setTextButton(9));
-        h10.setText(setTextButton(10));
-        h11.setText(setTextButton(11));
-        h12.setText(setTextButton(12));
+    private void setTextLabel() {
+        u2.setText(getUsernameFromHighScoreTable(2));
+        u3.setText(getUsernameFromHighScoreTable(3));
+        u4.setText(getUsernameFromHighScoreTable(4));
+        u5.setText(getUsernameFromHighScoreTable(5));
+        u6.setText(getUsernameFromHighScoreTable(6));
+        u7.setText(getUsernameFromHighScoreTable(7));
+        u8.setText(getUsernameFromHighScoreTable(8));
+        u9.setText(getUsernameFromHighScoreTable(9));
+        u10.setText(getUsernameFromHighScoreTable(10));
+        u11.setText(getUsernameFromHighScoreTable(11));
+        u12.setText(getUsernameFromHighScoreTable(12));
+
+        h2.setText(getHighScoreFromHighScoreTable(2));
+        h3.setText(getHighScoreFromHighScoreTable(3));
+        h4.setText(getHighScoreFromHighScoreTable(4));
+        h5.setText(getHighScoreFromHighScoreTable(5));
+        h6.setText(getHighScoreFromHighScoreTable(6));
+        h7.setText(getHighScoreFromHighScoreTable(7));
+        h8.setText(getHighScoreFromHighScoreTable(8));
+        h9.setText(getHighScoreFromHighScoreTable(9));
+        h10.setText(getHighScoreFromHighScoreTable(10));
+        h11.setText(getHighScoreFromHighScoreTable(11));
+        h12.setText(getHighScoreFromHighScoreTable(12));
+
     }
 
-    private String setTextButton(int id) {
-        return String.format("%s \n High Score: %d", highscoreDAO.getListFromColumn("multiplier", id).get(0).getUsername(),
-                highscoreDAO.getListFromColumn("multiplier", id).get(0).getScore());
+    private String getUsernameFromHighScoreTable(int id) {
+        return highscoreDAO.getListFromColumn("multiplier", id).get(0).getUsername();
+    }
+
+    private String getHighScoreFromHighScoreTable(int id) {
+        return String.valueOf(highscoreDAO.getListFromColumn("multiplier", id).get(0).getScore());
     }
 
     private String splitButtonName(String id) {
